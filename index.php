8e42f8e18d25b53d409aeadaae84d52fd873f2ad
Ardian1408 <95554197+Ardian1408@users.noreply.github.com>
2023-04-14 16:50:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Scrolling Nav - Start Bootstrap Template</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/Landpaging.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">VESTRY</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">Tentang</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Cara Kerja</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-primary bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Selamat Datang di Website VESTRY</h1>
+                <p class="lead">Butuh Modal Usaha? VESTRY Jawabannya!
+                    Dapatkan bantuan modal dengan proses yang mudah dan cepat tanpa khawatir bunga yang besar. Bergabung bersama kami untuk pengajuan pendanaan.
+                </p>
+                <a class="btn btn-lg btn-light" href="#about">Scroll ke bawah</a>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Apa itu VESTRY?</h2>
+                        <p class="lead">VESTRY adalah platform layanan pendanaan bersama berbasis teknologi yang mempertemukan mitra 
+                            (pelaku usaha di bidang agroindustri) dan pendana (pemilik dana). Platform ini memfasilitasi pengajuan pendanaan dengan 
+                            sistem bagi hasil yang dapat menguntungkan pihak mitra dan pendana. 
+                            VESTRY menawarkan berbagai kemudahan dalam proses pengajuan pendanaan dan pemberian dana melalui berbagai fitur pendukung antara lain</p>
+                        <ul>
+                            <li><h4>Deskripsi Usaha</h4></li>
+                            <p>Fitur yang mampu menunjang mitra dalam memperkenalkan usahanya kepada pendana. Melalui informasi yang tersedia pada deskripsi dapat membantu pendana untuk mempertimbangkan pemberian bantuan modal.</p>
+                            <li><h4>Proyek pendanaan</h4></li>
+                            <p>Terdiri dari berbagai fitur pendukung seperti e-contract, pembayaran, pencairan, progress serta bagi hasil yang dapat mempermudah baik dari sisi mitra maupun pendana.</p>
+                            <li><h4>Riwayat pendanaan</h4></li>
+                            <p>Ringkasan proyek pendanaan yang pernah dijalankan baik dari sisi mitra maupun pendana</p>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services section-->
+        <section class="bg-light" id="services">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Cara Kerja VESTRY</h2>
+                        <p class="lead">Mitra : Pelaku usaha di bidang agroindustri yang menggunakan VESTRY untuk mendapatkan tambahan modal usaha</p>
+                        <p class="lead">Admin VESTRY : Mengatur jalannya alur pendanaan dan maintenance sistem</p>
+                        <p class="lead">Pendana : Pemilik dana yang menyepakati kontrak kerja sama dengan mitra</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact section-->
+        <section id="contact">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2>Contact us</h2>
+                        <p class="lead">Kelompok B6.</p>
+                        <p class="lead">Fakultas Ilmu Komputer</p>
+                        <p class="lead">Universitas Jember</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
